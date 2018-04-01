20db3e7073987a4d4a370b98dd7688543ad8c356
Steven DiCarlo <stevendicarlo2@gmail.com>
2018-04-01 17:05:22 -0400

    Create method to calculate confidence intervals for normal data

--- chicken.py
+++ chicken.py
@@ -29,6 +29,71 @@ def get_move(state):
 def get_time(prior_info):
     return .2
 
+def get_normal_confidence_bound(data):
+    n = len(data)
+    avg = sum(data)/n
+    temp = 0
+    for point in data:
+        temp += (point-avg)**2
+    var = temp/(n-1)
+
+    chi2_table_90 = [2.706,
+                     4.605,
+                     6.251,
+                     7.779,
+                     9.236,
+                     10.645,
+                     12.017,
+                     13.362,
+                     14.684,
+                     15.987,
+                     17.275,
+                     18.549,
+                     19.812,
+                     21.064,
+                     22.307,
+                     23.542,
+                     24.769,
+                     25.989,
+                     27.204,
+                     28.412]
+    t_table_90 = [3.08,
+                  1.89,
+                  1.64,
+                  1.53,
+                  1.48,
+                  1.44,
+                  1.42,
+                  1.4,
+                  1.38,
+                  1.37,
+                  1.36,
+                  1.36,
+                  1.35,
+                  1.35,
+                  1.34,
+                  1.34,
+                  1.33,
+                  1.33,
+                  1.33,
+                  1.33,
+                  1.32,
+                  1.32,
+                  1.32,
+                  1.32,
+                  1.32,
+                  1.32,
+                  1.31,
+                  1.31,
+                  1.31,
+                  1.31]
+    sample_n = min(n, 30)
+    z_score = 1.28 if n > 31 else t_table_90[n-2]
+    var_upper_bound = var + (sample_n-1)*var/chi2_table_90[sample_n-2]
+    stddev_upper_bound = var_upper_bound**.5
+    mean_upper_bound = avg + z_score*((var/n)**.5)
+    return mean_upper_bound, stddev_upper_bound
+
 def load_data():
     sample_info = {
         "past_reaction_times": [.1, .12, .32],
@@ -43,3 +108,6 @@ def load_data():
 
 def save_data(info):
     return
+
+sample_data = [0, 1, 2, 3]
+print(get_normal_confidence_bound(sample_data))
