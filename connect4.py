76b322b72a07b9978ee1ea4a87914619d867cdcb
Steven DiCarlo <stevendicarlo2@gmail.com>
2018-04-01 12:56:20 -0400

    Create template for connect4

--- connect4.py
+++ connect4.py
@@ -1,2 +1,18 @@
 TEAM_NAME = "team"
 MEMBERS = ["snd7nb","yq4du"]
+
+def get_move(state):
+    cols = state["columns"]
+    n = state["connect_n"]
+    token = state["your-token"]
+    board = state["board"]
+
+    column_to_play = get_column_move(board, n, token)
+    team_code = state["team-code"]
+    return {
+        "move": column_to_play,
+        "team-code": team_code
+    }
+
+def get_column_move(board, n, token):
+    return 2
