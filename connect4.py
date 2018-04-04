6655244c2c488a890f02bb6dc2dea91ae59689a6
Steven DiCarlo <stevendicarlo2@gmail.com>
2018-04-04 13:20:05 -0400

    Create diagonal score 1 function

--- connect4.py
+++ connect4.py
@@ -129,6 +129,41 @@ def diagonal_win1(board, n, column, token):
         length += 1
     return length >= n
 
+def diagonal_score1(board, n, column, token):
+    if n > len(board):
+        return 0
+    last_col = len(board)-1
+    row = len(board[column])-1
+    left_edge = column
+    disp = 0
+    while left_edge >= 0 and column - left_edge <= n-1 and row+disp >= 0 and (get_chip_or_none(board, left_edge, row+disp) in [token, None]):
+        left_edge -= 1
+        disp -= 1
+    left_edge += 1
+    right_edge = column
+    disp = 0
+    while right_edge <= last_col and right_edge-column <= n-1 and (get_chip_or_none(board, right_edge, row+disp) in [token, None]):
+        right_edge += 1
+        disp += 1
+    right_edge -= 1
+
+    if right_edge-left_edge < n-1:
+        return 0
+
+    score = 0
+    for i in range(left_edge, left_edge+n, 1):
+        if get_chip_or_none(board, i, row+i-column) == token:
+            score += 1
+    max_score = score
+    for i in range(left_edge+n, right_edge+1, 1):
+        if get_chip_or_none(board, i, row+i-column) == token:
+            score += 1
+        if get_chip_or_none(board, i-n, row+(i-n)-column) == token:
+            score -= 1
+        if score > max_score:
+            max_score = score
+    return max_score/n
+
 def diagonal_win2(board, n, column, token):
     if n > len(board):
         return False
@@ -186,13 +221,13 @@ sample_state = {
     "your-token": "R",
     "board": [
         ["R"],
-        ["Y","R"],
-        ["Y"],
         ["Y", "R"],
+        ["Y", "Y"],
         ["Y"],
-        ["R", "R"],
+        ["R"],
+        ["R", "R", "Y", "Y", "Y"],
         ["R", "R"]
     ]
 }
 board = sample_state["board"]
-print(horizontal_score(board, 2, 5, "R"))
+print(diagonal_score1(board, 3, 3, "Y"))
